2ec0b30cfdaaf532f23a44fa8b1e38bfa16268b7
Marcelo <mllerena@tacdigital.pe>
2024-02-06 01:19:04 -0500

    fix: object

--- index.js
+++ index.js
@@ -18,42 +18,64 @@ addBtn.addEventListener('click', (e) => {
     const txtNote = note.value.trim();
 
     if (txtNote !== '') {
+        const uniqueId = Date.now().toString();
+        const noteObj = { id: uniqueId, content: txtNote };
+
         const li = document.createElement('li');
+        li.dataset.id = uniqueId;
         li.classList.add('old-note');
 
         const p = document.createElement('p');
         p.textContent = txtNote;
 
         li.appendChild(p);
-        li.appendChild(addDelBtn());
+        li.appendChild(addDelBtn(uniqueId));
+        li.appendChild(addEdit(uniqueId));
         noteList.appendChild(li);
 
-        saveNote(txtNote);
+        saveNote(noteObj);
         note.value = "";
+    }
+    else {
+        alert('Mensaje incorrecto, intentelo de nuevo');
     }
 });
 
 /* ---------- borrar nota ----------*/
 
-function addDelBtn() {
+function addDelBtn(noteId) {
     const delBtn = document.createElement('button');
     delBtn.classList.add('delete-btn');
     delBtn.textContent = 'Borrar';
 
     delBtn.addEventListener('click', (e) => {
-        const item = e.target.parentElement;
+        const itemId = e.target.parentElement.dataset.id;
+        const item = noteList.querySelector(`li[data-id="${itemId}"]`);
         noteList.removeChild(item);
-        delNote(item.querySelector('p').textContent);
+        delNote(itemId);
     });
 
     return delBtn;
 }
 
+function addEdit(noteId) {
+    const edBtn = document.createElement('button');
+    edBtn.classList.add('delete-btn');
+    edBtn.textContent = 'Editar';
+
+    edBtn.addEventListener('click', (e) => {
+        const itemId = e.target.parentElement.dataset.id;
+        const item = noteList.querySelector(`li[data-id="${itemId}"]`);
+        editNote(itemId);
+    });
+    return edBtn;
+}
+
 /* ---------- almacenamiento ----------*/
 
-function saveNote(textNote) {
+function saveNote(noteObj) {
     let notes = getNotes();
-    notes.push(textNote);
+    notes.push(noteObj);
     localStorage.setItem('notes', JSON.stringify(notes));
 }
 
@@ -64,21 +86,37 @@ function getNotes() {
 function loadNotes() {
     const notes = getNotes();
 
-    notes.forEach((textNote) => {
+    notes.forEach((noteObj) => {
         const li = document.createElement('li');
+        li.dataset.id = noteObj.id;
         li.classList.add('old-note');
 
         const p = document.createElement('p');
-        p.textContent = textNote;
+        p.textContent = noteObj.content;
 
         li.appendChild(p);
-        li.appendChild(addDelBtn());
+        li.appendChild(addDelBtn(noteObj.id));
+        li.appendChild(addEdit(noteObj.id));
         noteList.appendChild(li);
     });
 }
 
-function delNote(textNote) {
+function delNote(noteId) {
     let notes = getNotes();
-    notes = notes.filter((note) => note !== textNote);
+    notes = notes.filter((noteObj) => noteObj.id !== noteId);
     localStorage.setItem('notes', JSON.stringify(notes));
+}
+
+
+function editNote(noteId) {
+    let notes = getNotes();
+    let noteObj = notes.find((note) => note.id === noteId);
+    if (noteObj) {
+        const newText = prompt('Escribe el nuevo texto');
+        if (newText !== null) {
+            noteObj.content = newText;
+            localStorage.setItem('notes', JSON.stringify(notes));
+            location.reload();
+        }
+    }
 }
